76e5b361ee503b2675142f5bd64806d8a993fff6
TTTT-san <121175666+TTTT-san@users.noreply.github.com>
2022-12-22 16:26:13 +0100

    Add files via upload

---
+++ Esercizi Java 22-12-22/ConvertiSecondi/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static String convertSecondi(int s){
+        String risult = " ";
+
+        int secM= 60;
+        int secH = 3600;
+        int secD = 86400;
+
+       /* int d=s/secD;
+        int h=s/secH;
+        int m=s/secM;
+
+        s = s - d*secD+h*secH+m*secM;
+        */
+
+        int d = s/secD;
+        int stot = s%secD;
+        int h= stot/secH;
+        stot=stot%secH;
+        int m = stot/secM;
+        stot= stot%secM;
+
+
+
+        return "DD: "+ d+" HH: "+h +" mm" + m+ " s:"+stot;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("---Esercizio 5");
+        System.out.print("Secondi:");
+        int s = in.nextInt();
+        System.out.println(convertSecondi(s));
+    }
+}
