d619c9045b4b4a4cfe13188c2a28dd2996227e2c
TTTT-san <121175666+TTTT-san@users.noreply.github.com>
2023-01-11 10:51:14 +0100

    Update Main.java
    More complex but shorter

--- Esercizi Java 22-12-22_01CorsoJava/ConvertiSecondi/src/Main.java
+++ Esercizi Java 22-12-22_01CorsoJava/ConvertiSecondi/src/Main.java
@@ -3,36 +3,24 @@ import java.util.Scanner;
 public class Main {
 
 
-    public static String convertSecondi(int s){
-        String risult = " ";
-
-        int secM= 60;
-        int secH = 3600;
-        int secD = 86400;
-
-       /* int d=s/secD;
-        int h=s/secH;
-        int m=s/secM;
-
-        s = s - d*secD+h*secH+m*secM;
-        */
-
-        int d = s/secD;
-        int stot = s%secD;
-        int h= stot/secH;
-        stot=stot%secH;
-        int m = stot/secM;
-        stot= stot%secM;
-
-
-
-        return "-DD: "+ d+" -HH: "+h +" -mm: " + m+ " -s: "+stot;
-    }
-    public static void main(String[] args) {
+    public static void main(String[]args){
         Scanner in = new Scanner(System.in);
-        System.out.println("---Esercizio 5");
-        System.out.print("Inserire Secondi:");
+        System.out.println("Insrire secondi: ");
         int s = in.nextInt();
-        System.out.println(convertSecondi(s));
+        int d,h,m,mth;
+        mth=s/2500000; //secondS average month
+        d= (s-mth*2500000)/86400;
+        h=(s-mth*2500000-d*86400)/3600;
+        m=(s-mth*2500000-d*86400-h*3600)/60;
+        s-=(mth*2500000+d*86400+h*3600+m*60);
+
+
+       System.out.print("//Il risultato --> ");
+
+        System.out.println("| Month: "+mth
+                                    +"| Day: "+d
+                                    +"| Hour: "+h 
+                                    +"| min: "+m
+                                    +"| sec: "+s);
     }
 }
